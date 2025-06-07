40b46b519506b18ae3a597a9d8f7473e087dd3d3
u00031133 <u00031133@sise.com.pe>
2025-06-06 22:55:36 -0500

    PRISCILA

--- src/main/java/com/api/diversity/controller/ClienteController.java
+++ src/main/java/com/api/diversity/controller/ClienteController.java
@@ -3,32 +3,52 @@ package com.api.diversity.controller;
 import com.api.diversity.model.Cliente;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @GetMapping
-    public String getAllClientes() {
-        return "Lista de clientess";
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    @GetMapping("/listar")
+    public List<Cliente> listarClientes() {
+        return clientes;
     }
 
-    @GetMapping("/{id}")
-    public String getClienteById(@PathVariable Integer id) {
-        return "" + id;
+    @GetMapping("/buscar/{id}")
+    public Cliente buscarCliente(@PathVariable Integer id) {
+        return clientes.stream()
+                .filter(cliente -> cliente.getIdCliente().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
-    @PostMapping
-    public String createCliente(@RequestBody Cliente cliente) {
-        return "";
+    @PostMapping("/crear")
+    public Cliente crearCliente(@RequestBody Cliente cliente) {
+        cliente.setIdCliente(clientes.size() + 1);
+        clientes.add(cliente);
+        return cliente;
     }
 
-    @PutMapping("/{id}")
-    public String updateCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
-        return "" + id;
+    @PutMapping("/actualizar/{id}")
+    public Cliente actualizarCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        Cliente clienteActual = buscarCliente(id);
+        if (clienteActual != null) {
+            clienteActual.setNombre(cliente.getNombre());
+            clienteActual.setTelefono(cliente.getTelefono());
+            clienteActual.setDireccion(cliente.getDireccion());
+        }
+        return clienteActual;
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteCliente(@PathVariable Integer id) {
-        return "" + id;
+    @DeleteMapping("/eliminar/{id}")
+    public Cliente eliminarCliente(@PathVariable Integer id) {
+        Cliente clienteActual = buscarCliente(id);
+        if (clienteActual != null) {
+            clientes.remove(clienteActual);
+        }
+        return clienteActual;
     }
 }
