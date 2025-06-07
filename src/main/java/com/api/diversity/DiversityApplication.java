a83b0ef1be998c6bd5898e0042412021aac6e354
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-06 19:38:16 -0500

    Agregar archivos iniciales del proyecto Spring Boot, incluyendo configuración de Maven, clases de aplicación y pruebas. Se incluyen archivos de configuración como .gitignore y .gitattributes, así como el wrapper de Maven para facilitar la gestión de dependencias.

---
+++ src/main/java/com/api/diversity/DiversityApplication.java
@@ -0,0 +1,13 @@
+package com.api.diversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiversityApplication.class, args);
+	}
+
+}
