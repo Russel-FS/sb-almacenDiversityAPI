2977c961641527023dcefa45c6ab2f52c7886d46
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-06 20:13:50 -0500

    Refactorizar el controlador de categorías para devolver objetos en lugar de cadenas en los métodos de listar, buscar, crear, actualizar y eliminar. Se mejora la gestión de categorías y se optimiza el código.

--- src/main/java/com/api/diversity/controller/CategoriaController.java
+++ src/main/java/com/api/diversity/controller/CategoriaController.java
@@ -14,27 +14,41 @@ public class CategoriaController {
     private static List<Categoria> categorias = new ArrayList<Categoria>();
 
     @GetMapping("/listar")
-    public String listarCategorias() {
-        return "Lista de cateeeeeeee";
+    public List<Categoria> listarCategorias() {
+        return categorias;
     }
 
     @GetMapping("/buscar/{id}")
-    public String buscarCategoria(@PathVariable Integer id) {
-        return "" + id;
+    public Categoria buscarCategoria(@PathVariable Integer id) {
+        return categorias.stream()
+                .filter(categoria -> categoria.getIdCategoria() == id)
+                .findFirst()
+                .orElse(null);
     }
 
     @PostMapping("/crear")
-    public String crearCategoria(@RequestBody Categoria categoria) {
-        return "";
+    public Categoria crearCategoria(@RequestBody Categoria categoria) {
+        categoria.setIdCategoria(categorias.size() + 1);
+        categorias.add(categoria);
+        return categoria;
     }
 
     @PutMapping("/actualizar/{id}")
-    public String actualizarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
-        return " " + id;
+    public Categoria actualizarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
+        Categoria categoriaActual = buscarCategoria(id);
+        if (categoriaActual != null) {
+            categoriaActual.setNombreCategoria(categoria.getNombreCategoria());
+            categoriaActual.setDescripcion(categoria.getDescripcion());
+        }
+        return categoriaActual;
     }
 
     @DeleteMapping("/eliminar/{id}")
-    public String eliminarCategoria(@PathVariable Integer id) {
-        return "" + id;
+    public Categoria eliminarCategoria(@PathVariable Integer id) {
+        Categoria categoriaActual = buscarCategoria(id);
+        if (categoriaActual != null) {
+            categorias.remove(categoriaActual);
+        }
+        return categoriaActual;
     }
 }
