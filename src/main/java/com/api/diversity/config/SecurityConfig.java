1e6d636b9fefcdba9d6aea4dd9f5ff4e00734d63
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-17 00:52:14 -0500

    sdsdfsdf

--- src/main/java/com/api/diversity/config/SecurityConfig.java
+++ src/main/java/com/api/diversity/config/SecurityConfig.java
@@ -1,40 +1,25 @@
 package com.api.diversity.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import static org.springframework.security.config.Customizer.withDefaults;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import static org.springframework.security.config.Customizer.withDefaults; 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.api.diversity.service.CustomUserDetailsService;
-
+ 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
+ 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
-        return authBuilder.build();
-    }
-
+ 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
