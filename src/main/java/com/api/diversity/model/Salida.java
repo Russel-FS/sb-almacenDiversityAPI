014fcba25414951e9fd42c8fc7802e4695b1de82
Russel-Fs <russelfloressolano900@gmail.com>
2025-07-04 18:47:28 -0500

    Se eliminaron las importaciones de CreationTimestamp y UpdateTimestamp en la clase Entrada para simplificar el código y mejorar la claridad.

--- src/main/java/com/api/diversity/model/Salida.java
+++ src/main/java/com/api/diversity/model/Salida.java
@@ -2,8 +2,6 @@ package com.api.diversity.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
